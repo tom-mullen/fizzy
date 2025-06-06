0807cfc3d0e13815efff4813d3c336bc4568d004
Jorge Manrubia <jorge@37signals.com>
2025-06-06 09:21:45 +0200

    Format

--- test/models/command/filter_cards_test.rb
+++ test/models/command/filter_cards_test.rb
@@ -16,6 +16,6 @@ class Command::FilterCardsTest < ActionDispatch::IntegrationTest
   test "respect existing filters" do
     result = execute_command "#{@card_ids.join(",")}", context_url: "http://37signals.fizzy.localhost:3006/cards?collection_ids%5B%5D=#{collections(:writebook).id}"
 
-    assert_equal cards_path(collection_ids: [collections(:writebook).id], card_ids: @card_ids), result.url
+    assert_equal cards_path(collection_ids: [ collections(:writebook).id ], card_ids: @card_ids), result.url
   end
 end
