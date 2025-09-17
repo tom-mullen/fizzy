e6608748f6f0382c2655562260623bc196bb659f
Jorge Manrubia <jorge@hey.com>
2025-09-17 19:39:32 +0200

    Fix test

--- test/controllers/collections_controller_test.rb
+++ test/controllers/collections_controller_test.rb
@@ -34,12 +34,12 @@ class CollectionsControllerTest < ActionDispatch::IntegrationTest
         auto_close_period: 1.day,
         auto_reconsider_period: 2.days
       },
-      user_ids: users(:david, :jz).pluck(:id)
+      user_ids: users(:kevin, :jz).pluck(:id)
     }
 
     assert_redirected_to edit_collection_path(collections(:writebook))
     assert_equal "Writebook bugs", collections(:writebook).reload.name
-    assert_equal users(:david, :jz).sort, collections(:writebook).users.sort
+    assert_equal users(:kevin, :jz).sort, collections(:writebook).users.sort
     assert_equal 1.day, entropy_configurations(:writebook_collection).auto_close_period
     assert_equal 2.days, entropy_configurations(:writebook_collection).auto_reconsider_period
     assert_not collections(:writebook).all_access?
